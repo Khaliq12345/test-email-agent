5b7c05488ade2fc42de38561e8d45c8e7e743a19
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2024-04-10 20:18:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,25 @@ app = FastAPI()
 
 @app.get("/generate_email/")
 def get_generated_email_response(q: List[str] = Query(None)):
-    query_json = {
-        'email': q[0],
-        'prospect_email': q[1],
-        'subject': q[2]
-    }
-    email_info = email_gen.get_email_category(q[0])
+    email = q[0]
+    prospect_email = q[1]
+    subject = q[2]
+    email_info = email_gen.get_email_category(email)
     if email_info:
         if 'QUESTIONS' in email_info['Category']:
-            generated_email = email_gen.question_get_chain(q[0], q[1], q[2])
+            generated_email = email_gen.question_get_chain(email, prospect_email, subject)
             return {
                 'drafted_email': generated_email,
                 'category': email_info['Category']
             }
+        
+        elif 'JOB_OFFER/CONSULTING' in email_info['Category']:
+            email_summary = email_gen.consulting_chain(email, prospect_email, subject, needs_collected=email_info['all_needs_collected'])
+            return {
+                'drafted_email': email_summary,
+                'category': email_info['Category'],
+                'needs_collected': email_info['all_needs_collected']
+            }
+                
     else:
         return None
