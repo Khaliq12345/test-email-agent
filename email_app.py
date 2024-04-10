06c7d733c668b97810b1eb37cdf39c9621f3c486
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2024-04-10 20:17:58 +0100

    Update email_app.py

--- email_app.py
+++ email_app.py
@@ -7,7 +7,7 @@ from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from operator import itemgetter
 
 #QUESTIONS
-QUESTION_PROMPT_TEMPLATE = """
+QUESTION = """
 You are an email inbox manager for Synergize AI Team; Your goal is to help draft email response for the team.
 
 KNOWLEDGE: {faq}
@@ -20,6 +20,48 @@ EMAIL: ''' {email} '''
 
 ===
 GENERATE RESPONSE:
+"""
+
+#JOB_OFFER/CONSULTING
+CONSULTING = """
+You are an email inbox manager for Synergize AI Team; Your goal is to help summarize emails that are about Job offer or Consulting.
+And you must generate an email summary about the following:
+1. What's the problem the prospect is trying to solve? 
+2. Their budget
+
+===
+Info about email:
+NEW PROSPECT EMAIL: {email_address}
+SUBJECT: {subject}
+EMAIL: ''' {email} '''
+
+===
+GENERATE Summary:
+"""
+
+#JOB_OFFER/CONSULTING2
+CONSULTING = """
+
+ALL NEEDS COLLECTED: {needs_collected}
+
+You are an email inbox manager for Synergize AI Team; Your goal is to help with emais that are about Job offer or Consulting based on whether ALL NEEDS COLLECTED is YES or NO.
+
+If ALL NEEDS COLLECTED is YES, then you must generate an email summary about the following:
+1. What's the problem the prospect is trying to solve? 
+2. Their budget
+
+If ALL NEEDS COLLECTED is NO, then you must generate email response to the prospect to collect further info based on the following - 
+1. What's the problem the prospect is trying to solve? 
+2. Their budget?
+
+===
+Info about email:
+NEW PROSPECT EMAIL: {email_address}
+SUBJECT: {subject}
+EMAIL: ''' {email} '''
+
+===
+GENERATE SUMMARY or RESPONSE:
 """
 
 
@@ -44,7 +86,7 @@ class EmailGenerator:
         return faq_str
     
     def question_get_chain(self, email, prospect_email, subject):
-        PROMPT = ChatPromptTemplate.from_template(QUESTION_PROMPT_TEMPLATE)
+        PROMPT = ChatPromptTemplate.from_template(QUESTION)
         chain = (
             {
                 "faq": itemgetter('faq'),
@@ -63,5 +105,23 @@ class EmailGenerator:
         })
         return email_response
     
-
-
+    def consulting_chain(self, email, prospect_email, subject, needs_collected):
+        prompt = ChatPromptTemplate.from_template(CONSULTING)
+        chain = (
+            {
+                "needs_collected": itemgetter('needs_collected'),
+                'email_address': itemgetter('email_address'),
+                'subject': itemgetter('subject'),
+                'email': itemgetter('email'),
+            }
+            | prompt
+            | self.llm
+        )
+        email_response = chain.invoke({
+            "needs_collected": needs_collected,
+            'email_address': prospect_email,
+            'subject': subject,
+            'email': email,
+        })
+        return email_response
+            
